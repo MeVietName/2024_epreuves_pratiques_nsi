ae2d371aed54ceffd1ad9b95721c8d7b6df87f2b
MeVietName <thibautnemo@gmail.com>
2024-03-27 15:55:18 +0100

    1

---
+++ script.py
@@ -0,0 +1,19 @@
+#script to create folder contained : pdf + .py
+import os
+import shutil
+import glob
+ 
+os.chdir("2024_epreuves_pratiques_nsi")
+file_list = os.listdir()
+print(file_list)
+
+
+
+# os.makedirs(nom_du_fichier, )
+source_file = "D:\Programmes\2024_epreuves_pratiques_nsi"
+
+destination_file = "D:\Programmes"
+
+# shutil.copy()
+
+
