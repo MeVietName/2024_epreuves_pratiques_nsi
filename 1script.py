68234911dbc9ee932f1cee649466509e1f35d626
Nixuge <mail@nixuge.me>
2024-04-03 01:46:28 +0200

    Nouvelle version incroyable

--- 1script.py
+++ 1script.py
@@ -1,34 +1,25 @@
 #script to create folder contained : pdf + .py
 import os
 import shutil
-import glob
- 
-os.chdir("les copies")
-file_list = os.listdir()
-print(file_list)
-os.chdir("..")
-os.listdir()
 
-#directory name : 24_NSI_<nb>
-for i in range(47):
-    target_folder = file_list[i]
-    folder_name = target_folder.replace(".pdf", " ") #permet d'enlever l'extension dans le nom du fichier
+def padding(entree: int, nbre_caracteres: int):
+    entree_str = str(entree)
+    while len(entree_str) < nbre_caracteres:
+        entree_str = "0" + entree_str
+    return entree_str
+
+# directory name : 24_NSI_<nb>
+for i in range(1, 49):
+    num = padding(i, 2)
+    folder_name = "24_NSI_" + num
     print(folder_name)
     if not os.path.exists(folder_name):
         os.mkdir(folder_name)
     print(folder_name)
     #need to fix the path to move 
-    shutil.move(file_list[i], folder_name)
-    shutil.move(file_list[i+1], folder_name)
-    i=+1
-    
-    
+    shutil.move("les copies/24_NSI_" + num + ".pdf" , folder_name)
+    shutil.move("les copies/24_NSI_" + num + ".py" , folder_name)
 
-
-# os.makedirs(nom_du_fichier, )
-source_file = "D:\Programmes\2024_epreuves_pratiques_nsi"
-
-destination_file = "D:\Programmes"
 
 
 # https://note.nkmk.me/en/python-str-remove-strip/#remove-exact-match-string-replace
