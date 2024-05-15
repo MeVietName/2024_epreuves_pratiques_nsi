34ca8d96858ce9ab19d9424e382961b7f94e611d
MeVietname <thibautnemo@gmail.com>
2024-05-15 14:59:31 +0200

    f7 ex1

---
+++ Robert sante(sans t)/24_NSI_07/Exercice_1.py
@@ -0,0 +1,34 @@
+# def gb_vers_entier(tab):
+#     idk = len(tab)
+#     calcul = 0
+#     for i in range(idk):
+#         if tab[i] == True:
+#             calcul += 2**i
+#     idk -= 1
+#     return calcul
+
+# complique need de le refaire
+
+# Correction :
+def gb_vers_entier(tab):
+    valeur = 0
+    length = len(tab)
+    for i in range(length):
+        verif_bool = tab[length - 1 - i]
+        if verif_bool : 
+            valeur = valeur + 2**i
+    return valeur
+
+
+
+# 0
+print(gb_vers_entier([]))
+
+# 1
+print(gb_vers_entier([True]))
+
+# 83
+print(gb_vers_entier([True, False, True,False, False, True, True]))
+
+# 130
+print(gb_vers_entier([True, False, False, False,False, False, True, False]))
