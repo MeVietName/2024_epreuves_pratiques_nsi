2d86e0063cce4ca396777a10a692e29190e333fb
MeVietname <thibautnemo@gmail.com>
2024-05-15 19:02:56 +0200

    f10 ex1 finis mais pas opti du tout

--- Robert sante(sans t)/24_NSI_10/Exercice_1.py
+++ Robert sante(sans t)/24_NSI_10/Exercice_1.py
@@ -1,8 +1,15 @@
 def moyenne(notes) -> float:
     length = len(notes)
+    valeur = 0
+    nb_de_notes = 0
     for i in range(length):
-        valeur = valeur + notes[i][0] * notes[i][1]
-    return valeur // length
+        if notes[i][1] != 0:
+            valeur = valeur + notes[i][0] * notes[i][1]
+            nb_de_notes = nb_de_notes + notes[i][1]
+    if nb_de_notes !=0:   
+        return valeur / nb_de_notes
+    else:
+        return None
     
     
 # 9.142857142857142
