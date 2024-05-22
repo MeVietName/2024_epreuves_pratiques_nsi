34223933bb5ce54c074ce6f4a6e0d7ea378574db
MeVietname <thibautnemo@gmail.com>
2024-05-22 11:55:58 +0200

    f15 finis

--- Robert sante(sans t)/24_NSI_15/24_NSI_15.py
+++ Robert sante(sans t)/24_NSI_15/24_NSI_15.py
@@ -2,11 +2,29 @@ def binaire(a):
     '''convertit un nombre entier a en sa representation 
     binaire sous forme de chaine de caractères.'''
     if a == 0:
-        return ... 
-    bin_a = ... 
-    while ... : 
-        bin_a = ... + bin_a 
-        a = ... 
+        return "0"
+    bin_a = "" 
+    while a > 0:
+        # print(bin_a)
+        bin_a = str(a%2) + bin_a
+        # print(a)
+        a = a//2
     return bin_a
 
+# BIEN SE RAPPELER : 
+# print(3//2)   >> 1
+# print(3/2)    >> 1.5
 
+
+
+# '1010011'
+print(binaire(83))
+
+# '110'
+print(binaire(6))
+
+# '1111111'
+# print(binaire(127))
+
+# '0'
+# print(binaire(0))
