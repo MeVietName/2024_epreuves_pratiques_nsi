11f6b08119e1d3de83594f82ddd61a2c69550b6e
MeVietname <thibautnemo@gmail.com>
2024-05-22 21:05:34 +0200

    f25 finis

--- Robert sante(sans t)/24_NSI_25/24_NSI_25.py
+++ Robert sante(sans t)/24_NSI_25/24_NSI_25.py
@@ -1,14 +1,19 @@
 def separe(tab):
     '''Separe les 0 et les 1 dans le tableau tab'''
     gauche = 0
-    droite = ... 
+    droite = len(tab) - 1 # j'avais fait -1 au lieu de faire la longueur de la liste - 1
     while gauche < droite:
         if tab[gauche] == 0 :
-            gauche = ... 
+            gauche = gauche + 1
         else :
-            tab[gauche] = ... 
-            tab[droite] = ... 
-            droite = ... 
+            tab[gauche] = 0 
+            tab[droite] = 1 
+            droite = droite - 1
     return tab
 
 
+# [0, 0, 0, 0, 1, 1, 1, 1]
+print(separe([1, 0, 1, 0, 1, 0, 1, 0]))
+
+# [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+print(separe([1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 0]))
