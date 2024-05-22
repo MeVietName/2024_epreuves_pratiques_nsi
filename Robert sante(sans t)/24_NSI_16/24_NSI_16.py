554aec98190c754ada450c1cc22e5f60ad689149
MeVietname <thibautnemo@gmail.com>
2024-05-22 15:16:38 +0200

    f16

--- Robert sante(sans t)/24_NSI_16/24_NSI_16.py
+++ Robert sante(sans t)/24_NSI_16/24_NSI_16.py
@@ -1,16 +1,25 @@
 def echange(tab, i, j):
     '''Echange les éléments d'indice i et j dans le tableau tab.'''
-    temp = ... 
-    tab[i] = ... 
-    tab[j] = ... 
+    temp = tab[i] 
+    tab[i] = tab[j] 
+    tab[j] = temp
+    
 
 def tri_bulles(tab):
     '''Trie le tableau tab dans l'ordre croissant
     par la méthode du tri à bulles.'''
     n = len(tab)
-    for i in range(...): 
-        for j in range(...): 
-            if ... > ...: 
-                echange(tab, j, ...) 
+    for i in range(n-1): 
+        for j in range(n-1): # ? idk on the correction they wrote range(n-i-1) 
+            if tab[j] > tab[j+1]: 
+                echange(tab, j, j+1)
+    return tab
 
+# []
+print(tri_bulles([]))
 
+# [1, 2, 3, 3, 6, 7, 9]
+print(tri_bulles([9, 3, 7, 2, 3, 1, 6]))
+
+# [3, 4, 7, 9]
+print(tri_bulles([9, 7, 4, 3]))
--- Robert sante(sans t)/24_NSI_16/Exercice_1.py
+++ Robert sante(sans t)/24_NSI_16/Exercice_1.py
@@ -7,6 +7,18 @@ def ecriture_binaire_entier_positif(n: int)-> str:
         n = n//2
     return binaire
 
+'''
+Correction :
+def ecriture_binaire_entier_positif(n):
+    vbin = str(n%2)
+    n = n // 2
+    while n > 0:
+        vbin = str(n%2) + vbin
+        n = n //2
+    return vbin
+'''
+
+
 # '0'
 print(ecriture_binaire_entier_positif(0))
 
