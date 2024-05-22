cd963683754b30b3129cd7c96bd81cbb1d57c0ea
MeVietname <thibautnemo@gmail.com>
2024-05-22 16:02:24 +0200

    f18 exo1

---
+++ Robert sante(sans t)/24_NSI_18/Exerice_1.py
@@ -0,0 +1,14 @@
+def multiplication(n1, n2)-> int:
+    return n1*n2
+
+# 15
+print(multiplication(3, 5))
+
+# 32
+print(multiplication(-4, -8))
+
+# -12
+print(multiplication(-2, 6))
+
+# 0
+print(multiplication(-2, 0))
