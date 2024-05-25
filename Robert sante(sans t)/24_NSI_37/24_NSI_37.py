dac5a4a46788e7db122bb045c1fd129bb82df893
MeVietName <thibautnemo@gmail.com>
2024-05-25 13:21:55 +0200

    f37

--- Robert sante(sans t)/24_NSI_37/24_NSI_37.py
+++ Robert sante(sans t)/24_NSI_37/24_NSI_37.py
@@ -1,15 +1,19 @@
 def tri(tab):
     '''tab est un tableau d'entiers contenant des 0 et des 1.
     La fonction trie ce tableau en plaçant tous les 0 à gauche'''
-    i = ... # premier indice de la zone non triée 
-    j = ... # dernier indice de la zone non triée 
+    i = 0 # premier indice de la zone non triée 
+    j = len(tab) - 1 # dernier indice de la zone non triée //// bien pensera mettre la (longueur - 1)
     while i < j:
         if tab[i] == 0:
-            i = ... 
+            i = i + 1 
         else:
-            valeur = ... 
-            tab[j] = ... 
-            ...
-            j = ... 
+            valeur = tab[j] 
+            tab[j] = 1  #bien penser a mettre 1
+            tab[i] = valeur
+            j = j - 1
+    return tab
+
+# [0, 0, 0, 0, 0, 1, 1, 1, 1]
+print(tri([0,1,0,1,0,1,0,1,0]))
 
 
