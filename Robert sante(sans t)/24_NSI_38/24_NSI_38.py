10eddcbbb6316cca550359e422222e359c4df959
MeVietName <thibautnemo@gmail.com>
2024-05-25 13:34:34 +0200

    f38

--- Robert sante(sans t)/24_NSI_38/24_NSI_38.py
+++ Robert sante(sans t)/24_NSI_38/24_NSI_38.py
@@ -2,20 +2,27 @@ def renverse(pile):
     '''renvoie une pile contenant les mêmes éléments que pile,
     mais dans l'ordre inverse.
     Cette fonction détruit pile.'''
-    pile_inverse = ... 
+    pile_inverse = [] 
     while pile != []:
-        ... .append(...) 
-    return ... 
+        pile_inverse.append(pile.pop()) 
+    return pile_inverse 
 
-
+# bien revoir cette seconde fonction
 def positifs(pile):
     '''renvoie une pile contenant les éléments positifs de pile,
     dans le même ordre. Cette fonction détruit pile.'''
-    pile_positifs = ... 
+    pile_positifs = [] 
     while pile != []:
-        ... = pile.pop() 
-        if ... >= 0: 
-            ...
-    return ... 
+        v = pile.pop() 
+        if v >= 0: 
+            pile_positifs.append(v)
+    return renverse(pile_positifs)
 
+# [5, 4, 3, 2, 1]
+print(renverse([1, 2, 3, 4, 5]))
 
+# [0, 5, 4, 10, 9]
+print(positifs([-1, 0, 5, -3, 4, -6, 10, 9, -8]))
+
+# []
+print(positifs([-2]))
