c90c23fae136fe2cbb1fbed3a2dd48d67ba8cf24
MeVietName <thibautnemo@gmail.com>
2024-05-25 08:59:51 +0200

    f27 finis

--- Robert sante(sans t)/24_NSI_27/24_NSI_27.py
+++ Robert sante(sans t)/24_NSI_27/24_NSI_27.py
@@ -6,11 +6,25 @@ def colore_comp1(M, i, j, val):
 
     if i-1 >= 0: # propage à gauche
         colore_comp1(M, i-1, j, val)
-    if ... < len(M): # propage à droite 
-        colore_comp1(M, ..., j, val) 
-    if ...: # propage en haut 
-        colore_comp1(M, ..., ..., val) 
-    if ...: # propage en bas 
-        ...
+    if i+1 < len(M): # propage à droite 
+        colore_comp1(M, i+1, j, val) 
+    if j+1 < len(M): # propage en haut 
+        colore_comp1(M, i, j+1, val) 
+    if j-1 >= 0: # propage en bas 
+        colore_comp1(M, i, j-1, val)
+    return M
 
 
+# [[0, 0, 1, 0], 
+# [0, 3, 0, 1], 
+# [3, 3, 3, 0], 
+# [0, 3, 3, 0]]
+M = [[0, 0, 1, 0], 
+     [0, 1, 0, 1], 
+     [1, 1, 1, 0], 
+     [0, 1, 1, 0]]
+
+resultat = colore_comp1(M, 2, 1, 3)
+
+for i in resultat:
+    print(i)
