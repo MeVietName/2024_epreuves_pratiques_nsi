a48f0c87c6ff5dacc56342c2cf15410db183832b
MeVietName <thibautnemo@gmail.com>
2024-06-02 09:39:49 +0200

    easy

--- Robert sante(sans t)/24_NSI_12/Exercice_1.py
+++ Robert sante(sans t)/24_NSI_12/Exercice_1.py
@@ -1,14 +1,15 @@
-def tri_selection(tab):
-    taille = len(tab)
-    for i in range(taille-1):
-        minimum = i 
-        for k in range(i+1, taille): #taille ou taille-1 ?
-            if tab[k] < tab[minimum]:
-                minimum = k 
-        if minimum != i:
-            tab[i], tab[minimum] = tab[minimum], tab[i]
+def tri_selection(tab: list)-> list:
+    for i in range(len(tab)):
+        compa = i
+        for j in range(i+1, len(tab)):
+            if tab[j] < tab[compa]:
+                compa = j
+        tab[i], tab[compa] = tab[compa], tab[i]
+        
     return tab
             
+            
+        
 
 # [-9, 1, 6, 12, 52]
 print(tri_selection([1, 52, 6, -9, 12]))
