c9ca1184dd9ccf4640a14d60175b215cda4b7759
MeVietname <thibautnemo@gmail.com>
2024-06-02 17:05:40 +0200

    ca va

--- Robert sante(sans t)/24_NSI_29/24_NSI_29.py
+++ Robert sante(sans t)/24_NSI_29/24_NSI_29.py
@@ -1,17 +1,24 @@
 def ligne_suivante(ligne):
     '''Renvoie la ligne suivant ligne du triangle de Pascal'''
-    ligne_suiv = [...]
-    for i in range(...):
-            ligne_suiv.append(...)
-        ligne_suiv.append(...)
-    return ligne_suiv
+    ligne_suiv = [1] # 1 du debut
+    for i in range(1, len(ligne)): 
+        ligne_suiv.append(ligne[i-1] + ligne[i]) # On prend l'index -1 par rapport a celui que l'on veut calculer + celui avec le meme index
+    ligne_suiv.append(1) # 1 de fin
+    return ligne_suiv 
 
 def pascal(n):
     '''Renvoie le triangle de Pascal de hauteur n'''
     triangle = [ [1] ]
-    for k in range(...):
-        ligne_k = ...
+    for k in range(n): 
+        ligne_k = ligne_suivante(triangle[k])
         triangle.append(ligne_k)
     return triangle
 
+# [1, 4, 6, 4, 1]
+print(ligne_suivante([1, 3, 3, 1]))
 
+# [[1], [1, 1], [1, 2, 1]]
+print(pascal(2))
+
+# [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1]]
+print(pascal(3))
