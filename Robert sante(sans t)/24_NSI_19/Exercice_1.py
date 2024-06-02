5f5b307c848c630de5620710c309ac54acae78a7
MeVietname <thibautnemo@gmail.com>
2024-06-02 11:13:55 +0200

    finis

--- Robert sante(sans t)/24_NSI_19/Exercice_1.py
+++ Robert sante(sans t)/24_NSI_19/Exercice_1.py
@@ -1,11 +1,29 @@
-def liste_puissances(a : int, n : int)-> list:
-    resultat_liste = [a]
+def liste_puissances(a: int, n: int)-> list:
+    liste_calcul = [a]
     for i in range(1, n):
-        resultat_liste.append(a*resultat_liste[i-1])
-    return resultat_liste
-    
-
-
+        liste_calcul.append(a*liste_calcul[i-1])
+    return liste_calcul
+        
+def liste_puissances_borne(a: int, borne):
+    # if a >= borne: 
+    #     return []
+    # liste_calcul = [a]
+    # calcul = a
+    # while calcul < borne:
+    #     calcul = calcul * a
+    #     if calcul > borne:
+    #         return liste_calcul
+    #     liste_calcul.append(calcul)
+    # return liste_calcul
+    if a>=borne:
+        return []
+    liste = [1]
+    i=1
+    while a*liste[i-1]<borne:
+        liste.append(a*liste[i-1])
+        i += 1
+    return liste
+        
 
 # [3, 9, 27, 81, 243]
 print(liste_puissances(3, 5))
@@ -13,11 +31,10 @@ print(liste_puissances(3, 5))
 # [-2, 4, -8, 16]
 print(liste_puissances(-2, 4))
 
-# [2, 4, 8]
-print(liste_puissances(-2, 4))
+print("--------------")
 
-# # [2, 4, 8, 16]
-# print(liste_puissances_borne(2, 17))
+# [2, 4, 8, 16]
+print(liste_puissances_borne(2, 17))
 
-# # []
-# print(liste_puissances_borne(5, 5))
+# []
+print(liste_puissances_borne(5, 5))
