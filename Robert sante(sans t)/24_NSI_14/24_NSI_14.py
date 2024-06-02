f38bb1231659a93d8d7ce68350ad113e4a1f14f6
MeVietName <thibautnemo@gmail.com>
2024-06-02 10:19:08 +0200

    perfect

--- Robert sante(sans t)/24_NSI_14/24_NSI_14.py
+++ Robert sante(sans t)/24_NSI_14/24_NSI_14.py
@@ -1,16 +1,16 @@
 class Carte:
     def __init__(self, c, v):
-        """Initialise les attributs :
-        couleur (entre 1 et 4), 
-        valeur (entre 1 et 13). """
+        """Initialise les attributs couleur (entre 1 et 4), 
+        et valeur (entre 1 et 13). """
         self.couleur = c
         self.valeur = v
 
     def recuperer_valeur(self):
         """ Renvoie la valeur de la carte : 
         As, 2, ..., 10, Valet, Dame, Roi """
-        valeurs = ['As','2', '3', '4', '5', '6', '7', '8', '9', '10', 'Valet', 'Dame', 'Roi']
-        return valeurs[self.valeur - 1] 
+        valeurs = ['As','2', '3', '4', '5', '6', '7', '8', 
+                   '9', '10', 'Valet', 'Dame', 'Roi']
+        return valeurs[self.valeur - 1]
 
     def recuperer_couleur(self):
         """ Renvoie la couleur de la carte 
@@ -24,14 +24,24 @@ class Paquet_de_cartes:
         objets Carte possibles rangés par valeurs croissantes en
         commençant par pique, puis cœur, carreau et trèfle. """
         ...
-        ...
-            ...
+        for i in range(52):
+            
                 ...
 
     def recuperer_carte(self, pos):
         """ Renvoie la carte qui se trouve à la position pos 
         (entier compris entre 0 et 51). """
+        `...`
         ...
-        ...
+        
+jeu = Paquet_de_cartes()
+# "8 de coeur"
+carte1 = jeu.recuperer_carte(20)
+carte1.recuperer_valeur() + " de " + carte1.recuperer_couleur()
+carte2 = jeu.recuperer_carte(0)
 
+# "As de pique"
+carte2.recuperer_valeur() + " de " + carte2.recuperer_couleur()
 
+# AssertionError : paramètre pos invalide
+carte3 = jeu.recuperer_carte(52)
